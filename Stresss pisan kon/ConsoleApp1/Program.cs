58612b62f2d938d9a132e57169b0973a326742c5
Neze24 <159635909+Neze24@users.noreply.github.com>
2024-04-25 15:07:44 +0700

    Add files via upload

---
+++ Stresss pisan kon/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MyWindowApp
+{
+    public class MyForm : Form
+    {
+        public MyForm()
+        {
+            // Set properties of the form
+            Text = "My Window";
+            Width = 400;
+            Height = 300;
+
+            // Create controls
+            Button button = new Button();
+            button.Text = "Click me!";
+            button.Click += Button_Click;
+
+            // Add controls to the form
+            Controls.Add(button);
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button clicked!");
+        }
+
+        // Entry point of the application
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MyForm());
+        }
+    }
+}
